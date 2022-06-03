7ce834acfcd82d4e5918833f03d2d88060cabc67
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-06-03 20:42:30 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         double a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextDouble();         b=sc.nextDouble();         System.out.format("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        double a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextDouble();
+        b=sc.nextDouble();
+        System.out.format("%.2f",a*b);
+    }
+}
