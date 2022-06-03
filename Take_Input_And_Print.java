6213211eab22ac3b398ab731dce4bddf1e72dfcc
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-06-03 20:46:50 +0530

    import java.util.Scanner; class mr {     public static void main(String arjun[])     {         String a;         Scanner h=new Scanner(System.in);         a=h.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class mr
+{
+    public static void main(String arjun[])
+    {
+        String a;
+        Scanner h=new Scanner(System.in);
+        a=h.nextLine();
+        System.out.print(a);
+    }
+}
