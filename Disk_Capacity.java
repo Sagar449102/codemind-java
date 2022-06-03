7a6477a7d0dfa583a555744350e718e8e08677f7
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-06-03 20:50:08 +0530

    import java.util.Scanner; class mr {     public static void main(String arjun[])     {         int t,s,b;         Scanner hello=new Scanner(System.in);         t=hello.nextInt();         s=hello.nextInt();         b=hello.nextInt();         System.out.print(2*t*s*b*512);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class mr
+{
+    public static void main(String arjun[])
+    {
+        int t,s,b;
+        Scanner hello=new Scanner(System.in);
+        t=hello.nextInt();
+        s=hello.nextInt();
+        b=hello.nextInt();
+        System.out.print(2*t*s*b*512);
+    }
+}
