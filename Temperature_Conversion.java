c21a1dc1aba7a3738f4653aa38e89477719ee63f
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-06-03 20:37:04 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         float c,f;         Scanner sc=new Scanner(System.in);         c=sc.nextInt();         f=32+(c*9/5);         System.out.format("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    { 
+        float c,f;
+        Scanner sc=new Scanner(System.in);
+        c=sc.nextInt();
+        f=32+(c*9/5);
+        System.out.format("%.2f",f);
+    }
+}
