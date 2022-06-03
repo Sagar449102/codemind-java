3302a31783553528b305d61359e93b863e681f83
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-06-03 20:40:34 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         char ch;         int a;         Scanner sc=new Scanner(System.in);         ch=sc.next().charAt(0);         a=ch;         System.out.print(a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        char ch;
+        int a;
+        Scanner sc=new Scanner(System.in);
+        ch=sc.next().charAt(0);
+        a=ch;
+        System.out.print(a);
+    }
+}
